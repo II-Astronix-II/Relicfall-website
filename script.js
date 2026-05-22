430f68c2f6fea2cb4366884b2c1623cace28054d
II-Astronix-II <remi.jussac@gmail.com>
2026-05-22 13:58:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function downloadLatest() {
-
     const latestVersion = "RelicfallSetup.exe";
 
     alert("Téléchargement de la dernière version (1.0)");
-
     window.location.href = latestVersion;
 }
 
 function downloadGame() {
-
     const select = document.getElementById("version-select");
     const file = select.value;
 
     alert("Téléchargement de " + select.options[select.selectedIndex].text);
-
     window.location.href = file;
 }
-
